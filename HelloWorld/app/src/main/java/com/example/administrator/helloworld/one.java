985604e7f15e9a93f755870bb64aadcd1a1843df
Administrator <525730167@qq.com>
2020-03-17 21:26:31 +0800

    day02

--- HelloWorld/app/src/main/java/com/example/administrator/helloworld/one.java
+++ HelloWorld/app/src/main/java/com/example/administrator/helloworld/one.java
@@ -1,13 +1,44 @@
 package com.example.administrator.helloworld;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
-public class one extends AppCompatActivity {
+public class one extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_one);
+        Button button = (Button) findViewById(R.id.button1);
+        button.setOnClickListener(this);
+        Button button2 = (Button) findViewById(R.id.button2);
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(one.this, "Hello World2", Toast.LENGTH_SHORT).show();
+            }
+        });
+        Button button3 = (Button) findViewById(R.id.button3);
+        button3.setOnClickListener(new MyButtonClickListener());
+
+    }
+
+    class MyButtonClickListener implements View.OnClickListener {
+        @Override
+        public void onClick(View v) {
+            Toast.makeText(one.this, "Hello World3", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    @Override
+    public void onClick(View v) {
+        Toast.makeText(this, "Hello World", Toast.LENGTH_LONG).show();
+    }
+
+    public void bt4OnClick(View v) {
+        Toast.makeText(this, "Hello World4", Toast.LENGTH_LONG).show();
     }
 }
